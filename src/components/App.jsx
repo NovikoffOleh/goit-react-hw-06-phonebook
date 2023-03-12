92a32a81f94d7ba10c761c17b74f9ee952d0b4a1
NovikoffOleh <novikoff10000@gmail.com>
2023-03-12 12:30:59 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,7 +5,7 @@ import { Filter, ContactList, Section, ContactForm } from './index';
 export default function App() {
   return (
     <>
-      <Section title="ТелефонBook">
+      <Section title="ТелефонBook" > 
         <ContactForm />
       </Section>
       <Section title="Контакти">
