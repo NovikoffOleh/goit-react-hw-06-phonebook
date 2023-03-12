edc9e3636a5c12bf93821e9e2954f1a8a37f426e
NovikoffOleh <novikoff10000@gmail.com>
2023-03-12 12:38:39 +0200

    Update contactReducers.js

--- src/redux/contacts/contactReducers.js
+++ src/redux/contacts/contactReducers.js
@@ -6,7 +6,7 @@ const INITIAL_CONTACT_LIST = [
   {id: 'id-1', name: 'Григорій Сковорода', number: '(044)459-12-56'},
   {id: 'id-2', name: 'Тарас Бульба', number: '(050)443-89-12'},
   {id: 'id-3', name: 'Одарка Павленко', number: '(069)645-17-79'},
-  {id: 'id-4', name: 'Ілона Зріні', number: '(0412)227-91-26'},
+  {id: 'id-4', name: 'Ілона Зріні', number: '(0412)227-91-26'}
 ];
 
 const items = createReducer(INITIAL_CONTACT_LIST, {
